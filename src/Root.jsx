5601ae2543378b8920d9a47da43493013e732bd9
Sarwar <2plus7sarwar@gmail.com>
2023-04-12 05:29:30 +0600

    added footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const Root = () => {
     const navigation = useNavigation();
@@ -9,6 +10,7 @@ const Root = () => {
             <span className='flex justify-center'>{navigation.state === 'loading' && <span className='text-[#7E90FE] text-xl bg-gray-800 py-2 px-5 rounded-md absolute'>Loading...</span>}</span>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='mt-20 py-10 px-2 md:px-[10%] bg-[#1A1919] text-white/50'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 justify-between gap-5'>
+                <div className='flex flex-col gap-y-2 '>
+                    <h2 className='text-white text-3xl font-semibold'>FindCareer</h2>
+                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it.</p>
+                    <img className='w-[120px]' src="/social.png" alt="" />
+                </div>
+                <div className='flex flex-col gap-y-2 '>
+                    <h3 className='text-white text-xl font-semibold'>Company</h3>
+                    <p>About us</p>
+                    <p>Work</p>
+                    <p>Latest News</p>
+                    <p>Career</p>
+                </div>
+                <div className='flex flex-col gap-y-2 '>
+                    <h3 className='text-white text-xl font-semibold'>Product</h3>
+                    <p>Prototype</p>
+                    <p>Plans&Pricing</p>
+                    <p>Customers</p>
+                    <p>Integrations</p>
+                </div>
+                <div className='flex flex-col gap-y-2 '>
+                    <h3 className='text-white text-xl font-semibold'>Support</h3>
+                    <p>Help Desk</p>
+                    <p>Sales</p>
+                    <p>Become a Partner</p>
+                    <p>Developers</p>
+                </div>
+                <div className='flex flex-col gap-y-2 '>
+                    <h3 className='text-white text-xl font-semibold'>Contact</h3>
+                    <p>534 Broadway, NYC</p>
+                    <p>+777-978-5570</p>
+                </div>
+            </div>
+            <hr className='my-10 border-[#7E90FE]/20' />
+            <div className='flex justify-between'>
+                 <div><p>@2023 <span className='font-semibold'>FindCareer</span>. All rights Reserved</p></div>
+                 <div><p>Powered By <span className='font-semibold'>FindCareer</span></p></div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
