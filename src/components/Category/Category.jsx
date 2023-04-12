5382142a70b809a805a51026c23c9f03fab0430e
Sarwar <2plus7sarwar@gmail.com>
2023-04-12 06:06:19 +0600

    added some hover effects

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Category = ({category}) => {
     const{cat, jobs, img} = category;
     return (
-        <div className='bg-[#7E90FE]/10 rounded-lg  p-10'>
+        <div className='bg-[#7E90FE]/10 rounded-lg  p-10 shadow transition hover:shadow-md'>
             <img src={img} alt="" />
             <h1 className='text-xl text-[#474747] font-semibold mt-8 mb-2'>{cat}</h1>
             <p className='text-[#A3A3A3]'>{jobs} Jobs available</p>
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Job = ({job}) => {
     const {id, companyLogo, companyName, fulltimeOrPartTime, jobTitle, remoteOrOnsite, location, salary } = job;
     return (
-        <div className='flex flex-col gap-y-3 border-2 border-gray-200 p-8 rounded-md'>
+        <div className='flex flex-col gap-y-3 border-2 border-gray-200 p-8 rounded-md shadow transition hover:shadow-md'>
             <div className='w-[150px] h-[80px]'>
                 <img className='w-[100%]' src={companyLogo} alt="" />
             </div>
@@ -18,7 +18,7 @@ const Job = ({job}) => {
                 <span className='text-[#757575] font-medium flex'><img className='mr-2 w-6 h-6' src="/pin.png" alt="" /> {location}</span>
                 <span className='text-[#757575] font-medium flex'><img className='mr-2 w-6 h-6' src="/dollar.png" alt="" /> {salary}</span>
             </div>
-            <Link to={`/jobs/${id}`}><button className='w-[150px] text-white font-bold py-3 px-5 rounded bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>View Details</button></Link>
+            <Link to={`/jobs/${id}`}><button className='w-[150px] text-white font-bold py-3 px-5 rounded bg-gradient-to-r hover:bg-gradient-to-l transition-all from-[#7E90FE] to-[#9873FF]'>View Details</button></Link>
         </div>
     );
 };
